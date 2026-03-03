9ce49dc95b553832be03f1403038f2d7b3a98e3e
Marou-Fer <72912738+Marou-Fer@users.noreply.github.com>
2026-03-03 14:40:35 +0100

    Integrity

--- SAC_Custom_Widgets/Integrity Node Files/sign.js
+++ SAC_Custom_Widgets/Integrity Node Files/sign.js
@@ -20,5 +20,5 @@ const run = js => {
   'main.js',
   'styling.js'
 ].forEach(js => run(js))
-// main.js: sha256-MeTdl0DMaTJcaQnYlp5Tf96vDT+pdAewY19VdbZOs4Q=
-// styling.js: sha256-8AykY9bAoqb0rMOoG3kR6XtPhpLlmKLgIvznx/eeNio=
+
+
